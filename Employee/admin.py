b9f44394651c78f0cdff5e7df9be9207f2457d85
ReemAbdelhaleemHassan <es-reem.abdelhaleem2019@alexu.edu.eg>
2024-03-05 17:41:12 +0200

    add admin

--- Employee/admin.py
+++ Employee/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Employee)
+from django.contrib import admin
+from .models import Employee
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'branch', 'date_of_birth', 'work_system', 'period_of_work', 'joining_date', 'title', 'administration')
+    list_filter = ('branch', 'work_system', 'period_of_work', 'title', 'administration')
+    search_fields = ('name', 'name_in_english', 'national_id', 'title', 'administration')
+
+admin.site.register(Employee, EmployeeAdmin)
+
 
--- Employee/models.py
+++ Employee/models.py
@@ -1,19 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
 class Employee(models.Model):
     BRANCH_CHOICES = [
         ('branch1', 'Branch 1'),
         ('branch2', 'Branch 2'),
         ('branch3', 'Branch 3'),
         # Add more branches as needed
-    ]
-
-    EMPLOYEE_STATUS_CHOICES = [
-        ('single', 'Single'),
-        ('married', 'Married'),
-        ('divorced', 'Divorced'),
-        # Add more statuses as needed
     ]
 
     WORK_SYSTEM_CHOICES = [
@@ -43,7 +38,7 @@ class Employee(models.Model):
         ('admin3', 'Administration 3'),
         # Add more administrations as needed
     ]
-
+    
     SECTION_CHOICES = [
         ('section1', 'Section 1'),
         ('section2', 'Section 2'),
@@ -51,27 +46,31 @@ class Employee(models.Model):
         # Add more sections as needed
     ]
 
-    name = models.CharField(max_length=255)
-    name_in_english = models.CharField(max_length=255)
-    branch = models.CharField(max_length=50, choices=BRANCH_CHOICES)
-    employee_status = models.CharField(max_length=20, choices=EMPLOYEE_STATUS_CHOICES)
-    date_of_birth = models.DateField()
-    work_system = models.CharField(max_length=20, choices=WORK_SYSTEM_CHOICES)
-    period_of_work = models.CharField(max_length=20, choices=PERIOD_OF_WORK_CHOICES)
-    joining_date = models.DateField()
-    start_date = models.DateField(null=True, blank=True)
-    contract_expiry_date = models.DateField(null=True, blank=True)
-    national_id = models.CharField(max_length=20)
-    fingerprint_number = models.CharField(max_length=20, null=True, blank=True)
-    base_salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    insurance_subscription_fee = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    insurance_number = models.CharField(max_length=20, null=True, blank=True)
-    title = models.CharField(max_length=50, choices=TITLE_CHOICES)
-    administration = models.CharField(max_length=50, choices=ADMINISTRATION_CHOICES)
-    section = models.CharField(max_length=50, choices=SECTION_CHOICES)
-    subscribed_to_insurance = models.BooleanField(default=False)
-    subject_to_tax = models.BooleanField(default=False)
+    name = models.CharField(max_length=255, verbose_name="Name", null=True, blank=True)
+    name_in_english = models.CharField(max_length=255, verbose_name="Name in English", null=True, blank=True)
+    branch = models.CharField(max_length=50, choices=BRANCH_CHOICES, verbose_name="Branch")
+    date_of_birth = models.DateField(verbose_name="Date of Birth")
+    work_system = models.CharField(max_length=20, choices=WORK_SYSTEM_CHOICES, verbose_name="Work System")
+    period_of_work = models.CharField(max_length=20, choices=PERIOD_OF_WORK_CHOICES, verbose_name="Period of Work")
+    joining_date = models.DateField(verbose_name="Joining Date")
+    start_date = models.DateField(null=True, blank=True, verbose_name="Start Date")
+    contract_expiry_date = models.DateField(null=True, blank=True, verbose_name="Contract Expiry Date")
+    national_id = models.CharField(max_length=20, verbose_name="National ID")
+    fingerprint_number = models.CharField(max_length=20, null=True, blank=True, verbose_name="Fingerprint Number")
+    base_salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Base Salary")
+    insurance_subscription_fee = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Insurance Subscription Fee")
+    insurance_number = models.CharField(max_length=20, null=True, blank=True, verbose_name="Insurance Number")
+    title = models.CharField(max_length=50, choices=TITLE_CHOICES, verbose_name="Title")
+    administration = models.CharField(max_length=50, choices=ADMINISTRATION_CHOICES, verbose_name="Administration")
+    section = models.CharField(max_length=50, choices=SECTION_CHOICES, null=True, blank=True, verbose_name="Section")
+    subscribed_to_insurance = models.BooleanField(default=False, verbose_name="Subscribed to Insurance")
+    subject_to_tax = models.BooleanField(default=False, verbose_name="Subject to Tax")
+
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+
     
     
-    def __str__(self) -> str:
-        return self.name
+    # def __str__(self) -> str:
+    #     return self.name
